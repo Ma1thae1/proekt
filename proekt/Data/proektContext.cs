70368c1113adb062293cf4ceabf4d9fe72c352bd
Ma1thae1 <ilia141103@gmail.com>
2024-08-01 15:04:35 +0300

    Добавьте файлы проекта.

---
+++ proekt/Data/proektContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using proekt.Model;
+
+namespace proekt.Data
+{
+    public class proektContext : DbContext
+    {
+        public proektContext (DbContextOptions<proektContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<proekt.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ proekt/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using proekt.Data;
+using proekt.Model;
+
+namespace proekt.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly proekt.Data.proektContext _context;
+
+        public IndexModel(proekt.Data.proektContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
